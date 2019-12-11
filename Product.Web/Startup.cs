f05fc35955937c7a2913ea8472e07dedd634fdc7
mostafa abdelrasool <mostafa.abdelrasoul.89@gmail.com>
2019-12-11 17:30:46 +0200

    multiple startup project

--- Product.Web/Startup.cs
+++ Product.Web/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using EventBus;
 using EventBus.Abstractions;
 using EventBusRabbitMQ;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -17,6 +19,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Product.Persistance;
@@ -111,6 +114,26 @@ namespace Product.Web
                 return new DefaultRabbitMQPersistentConnection(factory, logger, retryCount);
             });
             RegisterEventBus(services);
+            #endregion
+            #region Authentication and Authorization
+            // Add JWT Bearer. This should be replaced when we have setup the Identity server
+            services
+              .AddAuthentication(options =>
+              {
+                  options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                  options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+              })
+              .AddJwtBearer(options =>
+              {
+                  options.TokenValidationParameters = new TokenValidationParameters()
+                  {
+                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetSection("AppConfiguration:Key").Value)),
+                      ValidAudience = Configuration.GetSection("AppConfiguration:SiteUrl").Value,
+                      ValidateIssuerSigningKey = true,
+                      ValidateLifetime = true,
+                      ValidIssuer = Configuration.GetSection("AppConfiguration:SiteUrl").Value
+                  };
+              });
             #endregion
         }
         private void RegisterEventBus(IServiceCollection services)
